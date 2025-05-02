0961d8ebff587f11f2944c90ef7e0757780cf283
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-02 08:59:39 -0700

    preparing demo to be submitted

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,5 +1,6 @@
 (function () {
-  const BASE_URL = 'https://nss-election.onrender.com/api';
+  // const BASE_URL = 'https://nss-election.onrender.com/api';
+  const BASE_URL = 'https://nss-e-demo.onrender.com/api';
   
 // ====== DOM ELEMENTS ======
 const loginPage = document.getElementById('login-page');
