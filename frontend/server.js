a69bad341fc34900b68cd6343b43d26c9195e5bf
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-04 17:57:33 -0700

    modified base socket uri

--- frontend/server.js
+++ frontend/server.js
@@ -1,27 +1,32 @@
-require('dotenv').config();
-const express = require('express');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const path = require('node:path');
-const cors = require('cors');
-const crypto = require('crypto');
-const fs = require('fs');
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const path = require("node:path");
+const cors = require("cors");
+const crypto = require("crypto");
+const fs = require("fs");
 
 const app = express();
 
 // Middleware to generate nonce for each request
 app.use((req, res, next) => {
-  const nonce = crypto.randomBytes(16).toString('base64');
+  const nonce = crypto.randomBytes(16).toString("base64");
   res.locals.nonce = nonce;
   next();
 });
-const baseURL = process.env.NODE_ENV==="production" ? process.env.BACKEND_URI : process.env.LOCAL_BACKEND_URI;
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? process.env.BACKEND_URI
+    : process.env.LOCAL_BACKEND_URI;
 
 // Apply CORS settings
-app.use(cors({
-  origin: baseURL,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: baseURL,
+    credentials: true,
+  })
+);
 
 // Apply Content Security Policy (CSP) with nonce
 app.use(
@@ -29,7 +34,7 @@ app.use(
     useDefaults: true,
     directives: {
       defaultSrc: ["'self'"],
-      connectSrc: ["'self'",  process.env.LOCAL_BACKEND_URI, "ws://localhost:5000"],
+      connectSrc: ["'self'", baseURL, "ws://localhost:5000"],
       styleSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
       scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
     },
@@ -41,51 +46,50 @@ app.use(
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 min
   max: 100,
-  message: 'Too many requests, please try again later.'
+  message: "Too many requests, please try again later.",
 });
 app.use(limiter);
 
 // Middleware to prevent directory traversal attacks
 // app.use('/socket.io-client', express.static(path.join(__dirname, 'node_modules/socket.io-client/socket.io.min.js')));
 // Serve static files like images, styles
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-  const requestedPath = path.resolve(path.join(__dirname, 'public', req.url));
-  if (!requestedPath.startsWith(path.join(__dirname, 'public'))) {
-    return res.status(403).send('Forbidden');
+  const requestedPath = path.resolve(path.join(__dirname, "public", req.url));
+  if (!requestedPath.startsWith(path.join(__dirname, "public"))) {
+    return res.status(403).send("Forbidden");
   }
   next();
 });
 
-
-
 // Serve frontend with nonce replacement
-app.get('/config', (req, res) => {
+app.get("/config", (req, res) => {
   // Send the baseUri and any other settings you want to expose
-  res.json({ baseUri:baseURL });
+  res.json({ baseUri: baseURL });
 });
 
-app.get('/', (req, res) => {
-  const htmlPath = path.join(__dirname, 'public', 'index.html');
+app.get("/", (req, res) => {
+  const htmlPath = path.join(__dirname, "public", "index.html");
 
-  fs.readFile(htmlPath, 'utf8', (err, data) => {
+  fs.readFile(htmlPath, "utf8", (err, data) => {
     if (err) {
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
       return;
     }
 
     // Replace placeholder __NONCE__ with actual nonce
     const finalHtml = data.replace(/__NONCE__/g, res.locals.nonce);
 
-    res.setHeader('Content-Security-Policy', `script-src 'self' 'nonce-${res.locals.nonce}'`);
+    res.setHeader(
+      "Content-Security-Policy",
+      `script-src 'self' 'nonce-${res.locals.nonce}'`
+    );
     res.send(finalHtml);
   });
 });
 
-
-
-app.get('/*index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/*index", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 5001;
