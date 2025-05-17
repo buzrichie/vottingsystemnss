368db2cb4f72949d5e7bf29f2f7b92cbbaa864bc
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-17 14:59:48 -0700

    Added router handler for /result

--- frontend/public/js/utils/router.js
+++ frontend/public/js/utils/router.js
@@ -1,9 +1,13 @@
 import { registerComponent } from "../../pages/register.js";
+import { adminShowResult } from "./adminShowResult.js";
 
 const app = document.getElementById("app");
 
 if (window.performance) {
   // Handle initial load OR refresh for /result
+  if (window.location.pathname === "/result") {
+    adminShowResult();
+  }
   if (window.location.pathname === "/register") {
     navigateTo(window.location.pathname);
   }
