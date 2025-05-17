34665cd02021a89b923a43f60416c6c8d881f61d
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-17 11:58:16 -0700

    Added view pages

---
+++ frontend/public/pages/home.js
@@ -0,0 +1,10 @@
+// home.js
+export function Home() {
+    return `
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is where your home page content goes.</p>
+      </div>
+    `;
+  }
+  
---
+++ frontend/public/pages/register.js
@@ -0,0 +1,5 @@
+export function registerComponent() {
+  return `
+    <h1>Welcome to register</h1>
+    `;
+}
